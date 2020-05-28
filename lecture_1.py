8c3b64cfdcc8339a886c89c21fce4b79e5f1b2ba
kamrankh068 <kamrankh068@gmail.com>
2020-05-28 14:26:47 +0530

    first lecture

---
+++ lecture_1.py
@@ -0,0 +1,55 @@
+#itexpert127001@gmail.com
+
+#python.org  3.7
+
+# sudo apt-get install python
+
+# Numbers, String, Lists, Dictionaries, Tuples, Set, Booleans,  Files
+
+
+#Variables are used to store values. 
+
+#Numbers 
+a = 1234 # int
+b= 3.1415 # float
+c= 3+4j #complex
+
+print(type(a))
+
+print(type(a),type(b),type(c))
+
+# Operators +(plus) , - (minus) ,*(mul) , /(div), //(floor div) , %(modulus)
+
+var_1 = 5
+var_2 = 2
+
+print("addition ", var_1 + var_2)
+
+print("Subtraction ", var_1 - var_2)
+
+print("Multiplication ", var_1 * var_2)
+
+print("Division ", var_1 / var_2)   #5/2 = 2.5
+
+print("Floor Division ", var_1 // var_2)  # 5//2 = 2
+
+print("Modulues " ,var_1 % var_2)  # 
+
+
+# 2 to the power 2
+sq_2 = 2 ** 3
+print("value of sq_2 ",sq_2) 
+
+var = 1234567.987765
+
+print("var %.3f" % var) # style 1
+
+print("var {:.2f}".format(var)) # style 2
+
+print("binary of 5 is ", bin(5))
+
+print("octal of 5 is ", oct(5))
+
+print("hex of 5 is ", hex(5))
+
+
