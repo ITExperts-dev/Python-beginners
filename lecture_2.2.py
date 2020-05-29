28c58c127a319ca8410d59ab40936da5d308e974
kamrankh068 <kamrankh068@gmail.com>
2020-05-29 20:15:37 +0530

    second lecture2.2

---
+++ lecture_2.2.py
@@ -0,0 +1,46 @@
+
+#"IT-EXPERTS FOUNDATION"
+
+# itexpert127001@gmail.com
+
+# operation on strings OR String Method
+
+Str = "python"
+
+#Capitalize
+print("Capitalize =", Str.capitalize())
+
+#Upper
+print("Upper =", Str.upper())
+
+str_2 = "PROGRAMMING IS EASY"
+
+#Lower
+print("Lower =",str_2.lower())
+
+#Title
+print("Title =", str_2.title())
+
+#Startwith
+print("Startwith =",Str.startswith('P'))
+print("Startwith =",Str.startswith('p'))
+
+
+#Replace
+print("Replace =", Str.replace('p','T'))
+
+#Find
+print("Find =" ,Str.find('o'))
+
+#Length
+print("Length =",len(Str))
+
+#Count
+print("count =", str_2.count('P'))
+
+#split()
+print("split =" ,str_2.split())
+
+Str_3 = 'python#is#good#language'
+
+print("Split # =", Str_3.split('#'))
